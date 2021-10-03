ec945cc1139bd664598507ae28bdb231bf10d284
Dunne, Rob (Data61, Eveleigh) <Rob.Dunne@data61.csiro.au>
2021-10-03 14:31:08 +1100

    determine.C.R

--- R/determine.C.R
+++ R/determine.C.R
@@ -3,64 +3,132 @@
 #' This function allows you to express your love of cats.
 #' @param f_fit object returned by f.fit
 #' @param df data frame 
+#' @param t1 initial estimates
 #' @param trace.plot Do you love cats? Defaults to TRUE.
-#' @param starting_valu Do you love cats? Defaults to TRUE.
+#' @param starting_value Do you love cats? Defaults to TRUE.
+#' @param start_at Do you love cats? Defaults to TRUE.
+#' @param debug  Do you love cats? Defaults to TRUE.
 #' #' @keywords cats
 #' @export
 #' @examples
 #' cat_function()
-determine.C <-function (f_fit,df,trace.plot=FALSE,starting_value=1) 
+determine.C<-function (f_fit, df, t1,trace.plot = FALSE, starting_value = 1,start_at=30,debug=debug) 
 {
     f <- f_fit$f.spline
-    x<- f_fit$x
-    y<-df$y
-    qq <- rep(0, 119)
-    if (starting_value==1){
-        xi <- 1
-        omega <- 2
-        lambda <- 1
-    }else{
-        mm1.df = minpack.lm::nlsLM(y ~ my.dsn(x,xi=xi, omega=omega, lambda=lambda), 
-                                   start = list(xi=1, omega=2, lambda= 1 ),
-                                   data = df,control=minpack.lm::nls.lm.control(maxiter=400))
-        
-        xi <- summary(mm1.df)$parameters[1,1]
-        omega <- summary(mm1.df)$parameters[2,1]
-        lambda <-  summary(mm1.df)$parameters[3,1] 
-    }
-    
-    for (ii in 15:119) {
+    x <- df$x
+    y <- df$y
+    qq <- rep(NA, 119)
+    ## if (starting_value == 1) {
+    ##     xi <- 1
+    ##     omega <- 2
+    ##     lambda <- 1
+    ## }
+    ## else {
+    ##     mm1.df = minpack.lm::nlsLM(y ~ my.dsn(x, xi = xi, omega = omega, 
+    ##         lambda = lambda), start = list(xi = mean(x), omega = 2, 
+    ##         lambda = 1), data = df, control = minpack.lm::nls.lm.control(maxiter = 400))
+    ##     xi <- summary(mm1.df)$parameters[1, 1]
+    ##     omega <- summary(mm1.df)$parameters[2, 1]
+    ##     lambda <- summary(mm1.df)$parameters[3, 1]
+    ## }
+
+    for (ii in start_at:119) {
         df2 <- df[1:ii, ]
-        cat("dim(df2)", dim(df2), "\n")
+        if(debug==TRUE){
+            cat("dim(df2)", dim(df2), "\n")
+            }
         mm1.df2 = minpack.lm::nlsLM(y ~ my.dsn(x, xi = xi, omega = omega, 
-            lambda = lambda), start = list(xi = xi, omega = omega, 
-            lambda = lambda), data = df2, control = minpack.lm::nls.lm.control(maxiter = 400))
+            lambda = lambda), start = list(xi = t1[1], omega = t1[2], 
+            lambda = t1[3]), data = df2, control = minpack.lm::nls.lm.control(maxiter = 400))
         xi <- summary(mm1.df2)$parameters[1, 1]
         omega <- summary(mm1.df2)$parameters[2, 1]
         lambda <- summary(mm1.df2)$parameters[3, 1]
         f0.1 <- predict(mm1.df2, newdata = df)
-#        plot(x,f0.1)
-#        f0.2 <- f0.1/(sum(diff(x)[1] * predict(mm1.df2, newdata = df))) #probably makes very li
-#        lines(x,f0.2)
+        f0.1 <- f0.1 + .Machine$double.eps
         ppp <- cumsum(f0.1) * diff(x)[1]
-
-#        hist(ppp)
-#        ppp<-sn::psn(x,  xi=summary(mm1.df2)$parameters[1,1], omega=summary(mm1.df2)$parameters[2,1], alpha= summary(mm1.df2)$parameters[3,1] )
-
-        if (trace.plot==TRUE){
-            plot(x,y,type="l",col="grey90",lwd=2,xlim=c(0,3))
-            lines(df2$x,df2$y,col="green",lwd=2)
-            lines(x,predict(mm1.df2,newdata=df),col="blue",lwd=3)  
+        if (trace.plot == TRUE) {
+            plot(x, y, type = "l", col = "grey90", lwd = 2, xlim = c(0,  range(x)[[2]]+0.05))
+            lines(df2$x, df2$y, col = "green", lwd = 2)
+            lines(x, predict(mm1.df2, newdata = df), col = "blue",       lwd = 3)
             system("sleep 1")
         }
-
         p0 <- propTrueNullByLocalFDR(ppp)
         f0 <- (sum(f) * f0.1)/sum(f0.1)
-        cat("p0 = ", p0, "\n")
+        if(debug==TRUE){
+            cat("p0 = ", p0, "\n")
+        }
         qq[ii] <- cumsum((-f_fit$counts * log(f0/(f))) - log(p0))[ii]
     }
     qq
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## determine.C <-function (f_fit,df,trace.plot=FALSE,starting_value=1) 
+## {
+##     f <- f_fit$f.spline
+##     x<- f_fit$x
+##     y<-df$y
+##     qq <- rep(0, 119)
+##     if (starting_value==1){
+##         xi <- 1
+##         omega <- 2
+##         lambda <- 1
+##     }else{
+##         mm1.df = minpack.lm::nlsLM(y ~ my.dsn(x,xi=xi, omega=omega, lambda=lambda), 
+##                                    start = list(xi=1, omega=2, lambda= 1 ),
+##                                    data = df,control=minpack.lm::nls.lm.control(maxiter=400))
+        
+##         xi <- summary(mm1.df)$parameters[1,1]
+##         omega <- summary(mm1.df)$parameters[2,1]
+##         lambda <-  summary(mm1.df)$parameters[3,1] 
+##     }
+    
+##     for (ii in 15:119) {
+##         df2 <- df[1:ii, ]
+##         cat("dim(df2)", dim(df2), "\n")
+##         mm1.df2 = minpack.lm::nlsLM(y ~ my.dsn(x, xi = xi, omega = omega, 
+##             lambda = lambda), start = list(xi = xi, omega = omega, 
+##             lambda = lambda), data = df2, control = minpack.lm::nls.lm.control(maxiter = 400))
+##         xi <- summary(mm1.df2)$parameters[1, 1]
+##         omega <- summary(mm1.df2)$parameters[2, 1]
+##         lambda <- summary(mm1.df2)$parameters[3, 1]
+##         f0.1 <- predict(mm1.df2, newdata = df)
+## #        plot(x,f0.1)
+## #        f0.2 <- f0.1/(sum(diff(x)[1] * predict(mm1.df2, newdata = df))) #probably makes very li
+## #        lines(x,f0.2)
+##         ppp <- cumsum(f0.1) * diff(x)[1]
+
+## #        hist(ppp)
+## #        ppp<-sn::psn(x,  xi=summary(mm1.df2)$parameters[1,1], omega=summary(mm1.df2)$parameters[2,1], alpha= summary(mm1.df2)$parameters[3,1] )
+
+##         if (trace.plot==TRUE){
+##             plot(x,y,type="l",col="grey90",lwd=2,xlim=c(0,3))
+##             lines(df2$x,df2$y,col="green",lwd=2)
+##             lines(x,predict(mm1.df2,newdata=df),col="blue",lwd=3)  
+##             system("sleep 1")
+##         }
+
+##         p0 <- propTrueNullByLocalFDR(ppp)
+##         f0 <- (sum(f) * f0.1)/sum(f0.1)
+##         cat("p0 = ", p0, "\n")
+##         qq[ii] <- cumsum((-f_fit$counts * log(f0/(f))) - log(p0))[ii]
+##     }
+##     qq
+## }
 ## determine.C <-function(f_fit){
 ##     f<-f_fit$f.spline
 ##     qq<-rep(0,119)
