042480779139ddf4e6b621349c1a15d41714d22a
Dunne, Rob (Data61, Eveleigh) <Rob.Dunne@data61.csiro.au>
2021-10-03 17:16:54 +1100

    changed local.fdr

--- R/local.fdr.R
+++ R/local.fdr.R
@@ -2,12 +2,13 @@
 #'
 #' This function allows you to express your love of cats.
 #' @param f  f 
+#' @param x  x 
 #' @param FUN FUN
 #' @keywords cats
 #' @export
 #' @examples
 #' local.fdr()
-local.fdr <-function(f,FUN=dburr, p0= 1, ...){
+local.fdr <-function(f,x,FUN=dburr, p0= 1, ...){
     f0<-FUN(x, ...)
     f<-f$f.spline
     f <- (sum(f0) * f)/sum(f)
