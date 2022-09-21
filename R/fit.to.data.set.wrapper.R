fa625b60f30f693e7035b82fc2594b1566ac7704
Rob Dunne <rob.dunne@data61.csiro.au>
2022-09-21 21:32:20 +1000

    added  R/fit.to.data.set.wrapper.R

---
+++ R/fit.to.data.set.wrapper.R
@@ -0,0 +1,20 @@
+#' fit.to.data.set.wrapper
+#'
+#' This function allows you to express your love of cats.
+#' @param f_fit object returned by f.fit
+#' @param imp importances
+#' @param debug.flag debug flag
+#' #' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
+fit.to.data.set.wrapper<-function(df,imp,debug.flag=0,plot.string="",temp.dir=temp.dir,try.counter=3){
+
+
+    mm.df1<-fit.to.data.set(df,imp,debug.flag=0,plot.string="",temp.dir=temp.dir,try.counter=1,return.all=TRUE)
+    mm.df2<-fit.to.data.set(df,imp,debug.flag=0,plot.string="",temp.dir=temp.dir,try.counter=2,return.all=TRUE)
+    mm.df3<-fit.to.data.set(df,imp,debug.flag=0,plot.string="",temp.dir=temp.dir,try.counter=3,return.all=TRUE)
+
+    mm.df3
+
+}
