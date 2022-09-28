0b338f18de6ae3d00d7d69f813a80f71b18c9e4f
Rob Dunne <rob.dunne@data61.csiro.au>
2022-09-29 09:25:48 +1000

    modified fit.to.data.set.wrapper

--- R/fit.to.data.set.wrapper.R
+++ R/fit.to.data.set.wrapper.R
@@ -8,13 +8,34 @@
 #' @export
 #' @examples
 #' cat_function()
-fit.to.data.set.wrapper<-function(df,imp,debug.flag=0,plot.string="",temp.dir=temp.dir,try.counter=3){
-
-
-    mm.df1<-fit.to.data.set(df,imp,debug.flag=0,plot.string="",temp.dir=temp.dir,try.counter=1,return.all=TRUE)
-    mm.df2<-fit.to.data.set(df,imp,debug.flag=0,plot.string="",temp.dir=temp.dir,try.counter=2,return.all=TRUE)
-    mm.df3<-fit.to.data.set(df,imp,debug.flag=0,plot.string="",temp.dir=temp.dir,try.counter=3,return.all=TRUE)
-
-    mm.df3
-
+fit.to.data.set.wrapper <- function (df, imp, debug.flag = 0, plot.string = "", temp.dir = temp.dir,
+                                        try.counter = 3){
+    mm.df1 <- fit.to.data.set(df, imp, debug.flag = 0, plot.string = "",
+                              temp.dir = temp.dir, try.counter = 1, return.all = TRUE)
+    mm.df2 <- fit.to.data.set(df, imp, debug.flag = 0, plot.string = "",
+                              temp.dir = temp.dir, try.counter = 2, return.all = TRUE)
+    mm.df3 <- fit.to.data.set(df, imp, debug.flag = 0, plot.string = "",
+                              temp.dir = temp.dir, try.counter = 3, return.all = TRUE)
+    if ( class(mm.df3) == "try-error"){
+        if ( class(mm.df1) == "try-error"){
+            if ( class(mm.df2) == "try-error"){
+                return("try-error")
+            } else {
+                return(  mm.df2)
+            }
+        }
+        else{ return( mm.df1)}
+    } else{
+        return( mm.df3)
+    }
 }
+
+## fit.to.data.set.wrapper<-function(df,imp,debug.flag=0,plot.string="",temp.dir=temp.dir,try.counter=3){
+
+##     mm.df1<-fit.to.data.set(df,imp,debug.flag=0,plot.string="",temp.dir=temp.dir,try.counter=1,return.all=TRUE)
+##     mm.df2<-fit.to.data.set(df,imp,debug.flag=0,plot.string="",temp.dir=temp.dir,try.counter=2,return.all=TRUE)
+##     mm.df3<-fit.to.data.set(df,imp,debug.flag=0,plot.string="",temp.dir=temp.dir,try.counter=3,return.all=TRUE)
+
+##     mm.df3
+
+## }
